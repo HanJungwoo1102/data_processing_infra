c162e0df5cbe286e3f558d9f618fcd665c1049e1
HanJungwoo1102 <ajtwlswjddnv1102@gmail.com>
2022-07-19 14:55:33 +0900

    feat:change the bucket names

--- data_processing_infra/s3.tf
+++ data_processing_infra/s3.tf
@@ -1,11 +1,11 @@
 resource "aws_s3_bucket" "temp_source" {
-    bucket = "temp_source_bucket"
+    bucket = "${var.pre_tag_name}-temp-source-bucket"
 
     tags = { Name = "${var.pre_tag_name}-s3-bucket-temp-source" }
 }
 
 resource "aws_s3_bucket" "l0" {
-    bucket = "l0_bucket"
+    bucket = "${var.pre_tag_name}-l0-bucket"
 
     tags = { Name = "${var.pre_tag_name}-s3-bucket-l0" }
 }
