f82232521e68910024623a734b87d10871219e84
HanJungwoo1102 <ajtwlswjddnv1102@gmail.com>
2022-08-01 08:53:56 +0900

    feat:add data management load balancer

--- data_processing_infra/load_balancer.tf
+++ data_processing_infra/load_balancer.tf
@@ -61,3 +61,40 @@ resource "aws_lb_listener_rule" "asg_api_server" {
         target_group_arn = aws_lb_target_group.asg_api_server.arn
     }
 }
+
+############################################
+
+resource "aws_lb" "data_management" {
+    name               = "${var.pre_tag_name}-alb-data-management"
+    internal           = false
+    load_balancer_type = "application"
+    security_groups    = [aws_security_group.alb.id]
+    subnets            = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+
+    tags = { Name = "${var.pre_tag_name}-alb-api-server" }
+}
+
+resource "aws_alb_target_group" "data_management" {
+    name     = "${var.pre_tag_name}-albtg-data-management"
+    port     = 80
+    protocol = "HTTP"
+    vpc_id   = aws_vpc.vpc.id
+
+    health_check {
+        path                = "/health-check"
+        protocol            = "HTTP"
+        matcher             = "200"
+        interval            = 15
+        timeout             = 3
+        healthy_threshold   = 2
+        unhealthy_threshold = 2
+    }
+
+    tags = { Name = "${var.pre_tag_name}-albtg-data-management" }
+}
+
+resource "aws_alb_target_group_attachment" "data_management" {
+    target_group_arn = aws_alb_target_group.data_management.arn
+    target_id        = aws_instance.data_management.id
+    port             = 80
+}
