cc1554c43ba8e0ce0fde372a735971c6c5ce0bdb
HanJungwoo1102 <ajtwlswjddnv1102@gmail.com>
2022-08-04 14:18:30 +0900

    feat:java 설치, gradle 설치 code 추가

--- data/initialize_ec2_instance.sh
+++ data/initialize_ec2_instance.sh
@@ -27,3 +27,22 @@ sudo mv docker-compose-$(uname -s)-$(uname -m) /bin/docker-compose
 sudo chmod -v +x /bin/docker-compose
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
+
+echo "#[Java Install]================================================================="
+wget https://download.oracle.com/java/17/archive/jdk-17.0.4_linux-x64_bin.tar.gz
+tar xvf jdk-17.0.4_linux-x64_bin.tar.gz
+sudo mv jdk-17.0.4 /opt/
+sudo tee /etc/profile.d/jdk.sh << EOF
+export JAVA_HOME=/opt/jdk-17.0.4
+export PATH=\$PATH:\$JAVA_HOME/bin
+EOF
+source /etc/profile.d/jdk.sh
+
+echo "#[Gradle Install]==============================================================="
+wget https://services.gradle.org/distributions/gradle-7.5-bin.zip
+sudo unzip gradle-*.zip -d /opt/gradle
+sudo tee /etc/profile.d/gradle.sh << EOF
+export GRADLE_HOME=/opt/gradle/gradle-7.5
+export PATH=\$PATH:\$GRADLE_HOME/bin
+EOF
+source /etc/profile.d/gradle.sh
